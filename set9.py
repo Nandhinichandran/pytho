51ae63a6a3251c0d34277582a0d2d505e5802dc0
Nandhini <32250902+Nandhinichandran@users.noreply.github.com>
2018-01-30 16:15:34 +0530

    Create set9.py

---
+++ set9.py
@@ -0,0 +1,12 @@
+x=int(input())
+y=int(input())
+count=0
+for n in range(x,y+1):
+    if(n>1):
+        for i in range(2,n):
+            if(n%i==0):
+                break
+        else:
+            count+=1
+print(count)
+            
